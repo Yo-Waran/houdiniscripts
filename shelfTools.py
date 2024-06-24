a6087a06a0c15cfc62c41c891f5d7b3a1f08e0e0
Ram Yogeshwaran <ramyogeshwaran.yogi@gmail.com>
2024-06-24 11:04:34 +0530

    Update shelfTools

--- shelfTools.py
+++ shelfTools.py
@@ -19,7 +19,7 @@ else:
 
 
 
-"""Run the Code"""
+"""Run the Selected Code that's stored"""
 #SET SHORTCUT TO Alt+Ctrl+M
 
 # Retrieve the stored file path and execute the script
@@ -81,3 +81,42 @@ openfile = hou.readFile(file) #opens the file
 
 #run the file
 exec(openfile)
+
+
+
+"""Auotmatically Run the Latest Saved VSCode file from its directory"""
+
+# Retrieve the stored file path and execute the script
+import os
+import glob
+
+# Replace with the directory where your VSCode files are located
+vscode_workspace_dir = '/jobs/tvcResources/bangComms/waranr/Scripts/Git_Repository/houdiniscripts'
+
+# Get a list of all files in the workspace directory
+files = glob.glob(os.path.join(vscode_workspace_dir, '*'))
+
+if files:
+    # Get the latest saved file in the workspace directory
+    latest_file = max(files, key=os.path.getctime)
+    
+    
+    print('Latest file path  : ' + latest_file)
+else:
+    print('No files found ')
+
+#run the latest saved file
+
+try:
+    # Retrieve the stored file path from Houdini's session variable
+    external_script_path = latest_file
+    
+    # Check if the path is not empty
+    if external_script_path:
+        # Open and execute the script
+        with open(external_script_path, "r") as file:
+            exec(file.read())
+    else:
+        hou.ui.displayMessage("No file path stored.")
+except AttributeError:
+    hou.ui.displayMessage("No file path stored. Please use the 'Store File Path' tool first.")
